5c1b86b8aca9ce700f1fe0ac4a1a6a39289f798c
Amir Daryabak <amir.daryabak@gmail.com>
2020-11-04 10:05:10 +0330

    Update 12 (added deepLink to FirstFragmentViewPager2.kt)

--- app/src/main/java/com/amirdaryabak/templateapp/ui/fragments/FirstFragmentViewPager2.kt
+++ app/src/main/java/com/amirdaryabak/templateapp/ui/fragments/FirstFragmentViewPager2.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import androidx.viewpager2.widget.ViewPager2
 import com.amirdaryabak.templateapp.R
 import com.amirdaryabak.templateapp.adapters.ViewPagerAdapter
@@ -11,17 +12,21 @@ import com.amirdaryabak.templateapp.ui.viewmodels.MainViewModel
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
+import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.fragment_first.*
 
 @AndroidEntryPoint
 class FirstFragmentViewPager2 : Fragment(R.layout.fragment_first) {
 
     private val viewModel: MainViewModel by viewModels()
+    private val args: FirstFragmentViewPager2Args by navArgs()
 
     val TAG = "FirstFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Toasty.success(requireContext(), args.name.toString()).show()
 
         val fragments = arrayListOf(
             SecondFragmentNotifications(),
